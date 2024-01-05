5f3687ed97796fe47acea6eecbe4a68f6b623a58
ngm01 <nathaniel.g.moore@gmail.com>
2024-01-04 21:23:20 -0500

    carousel movement

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
 
-export default function Card({title, img} : {title: string, img: string}) {
+export default function Card({title, img, isCurrent} : {title: string, img: string, isCurrent: boolean}) {
     return (
-        <main className="relative flex flex-col h-[600px] w-[300px] mx-3 px-10 bg-slate-50">
+        <main className={`${isCurrent ? 'border-8 border-white' : ''} relative flex flex-col h-[700px] w-[350px] mx-2 px-10 bg-slate-50`}>
             <Image alt="poster" fill style={{"objectFit": "cover"}} src={`/placeholders/${img}`}/>
-            <div className="text-red-700 text-center">{title}</div>
+            <div className="absolute flex justify-center bottom-8 left-0 right-0 text-2xl z-50 text-white text-center">{title}</div>
+            <div className={`absolute right-0 ${isCurrent ? 'h-[684px] w-[334px]' : 'h-[700px] w-[350px]'} bg-gradient-to-b from-transparent to-black to-97%`}></div>
         </main>
     )
 }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,6 +11,7 @@ export default function Carousel() {
         img: string
     }
     const [cards, setCards] = useState<Card[]>([])
+    const [currentSelection, setCurrent] = useState<number>(0)
 
     useEffect(() => {
         const exampleCards = [
@@ -23,9 +24,25 @@ export default function Carousel() {
         setCards(exampleCards)
     }, [])
 
+    function moveCarousel(direction: string) {
+        if(direction === 'left') {
+            if(currentSelection > 0) {
+                setCurrent(current => current - 1)
+            }
+        } else if(direction === 'right') {
+            if(currentSelection < cards.length - 1) {
+                setCurrent(current => current + 1)
+            }
+        }
+    }
+
     return (
-        <main className="flex flex-row bg-green-500">
-            {cards.map(card => <Card key={card.id} title={card.title} img={card.img} />)}
+        <main className="flex flex-row bg-black">
+            <div onClick={() => {moveCarousel('left')}} className="absolute top-1/2 left-10 z-50">Left arrow</div>
+            <div className="flex flex-row">
+                {cards.map(card => <Card isCurrent={card.id === cards[currentSelection].id} key={card.id} title={card.title} img={card.img} />)}
+            </div>
+            <div onClick={() => {moveCarousel('right')}} className="absolute top-1/2 right-10 z-50">Right arrow</div>
         </main>
     )
 }
