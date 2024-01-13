06a3968d3a118689d627b5db477ca6d191960515
ngm01 <nathaniel.g.moore@gmail.com>
2024-01-12 22:55:30 -0500

    fixed width for carousel, added emoji favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Cinema Antique',
+  title: 'Cinema Antique -- Watch Classic Films in the Public Domain',
   description: 'Public domain films',
 }
 
@@ -16,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📽️</text></svg>"
+      />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,7 +11,6 @@ export default function Carousel() {
         img: string
     }
     const [cards, setCards] = useState<Card[]>([])
-    const [currentSelection, setCurrent] = useState<number>(0)
     const [cardRange, setRange] = useState<number[]>([0, 3])
 
     useEffect(() => {
@@ -40,13 +39,12 @@ export default function Carousel() {
                 setRange(current => current.map((edge) => {return edge + 1}))
             }
         }
-        console.log(cardRange);
     }
 
     return (
         <main className="flex flex-row bg-black">
             <a href="#" onClick={() => {moveCarousel('left')}} className="fixed top-1/2 left-10 p-2 z-50 bg-white text-black">Left arrow</a>
-            <div className="flex flex-row">
+            <div className="flex flex-row w-[90rem] justify-start">
                 {cards.map((card, i) => {
                     if(i >= cardRange[0] && i <= cardRange[1]) {
                         return <Card isCurrent={i === cardRange[0]} key={card.id} title={card.title} img={card.img} />
